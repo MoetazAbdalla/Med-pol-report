f3369ab3a1c4d6dfaff8510236d0c5418d3bfa84
MoetazAbdalla <moetaz.abdalla@medipol.edu.tr>
2024-11-19 16:55:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -281,5 +281,5 @@ def update_sidebar(pathname):
 
 
 # Run the app
-if __name__ == "__main__":
-    app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0', port=8050)
