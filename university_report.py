70d1988253aebb65236f9ce753d5e7e9b9349f55
MoetazAbdalla <moetaz.abdalla@medipol.edu.tr>
2024-11-19 17:25:16 +0300

    Update university_report.py

--- university_report.py
+++ university_report.py
@@ -282,6 +282,7 @@ def register_callbacks(app):
 app.layout = layout
 register_callbacks(app)
 
+# Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run_server(debug=True)
 
