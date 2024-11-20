91cc738af79ba392c5e8b49f0ac5f260d71b8fdc
MoetazAbdalla <moetaz.abdalla@medipol.edu.tr>
2024-11-20 10:44:53 +0300

    Update Avg_reveno.py

--- Avg_reveno.py
+++ Avg_reveno.py
@@ -3,6 +3,8 @@ from dash import dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
 
 # Tuition fees data
 
