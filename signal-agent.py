dffbb5b1e8ca556d73c7f65c18572e388916c1fa
MoetazAbdalla <moetaz.abdalla@medipol.edu.tr>
2024-11-21 09:45:59 +0300

    Update signal-agent.py

--- signal-agent.py
+++ signal-agent.py
@@ -286,4 +286,5 @@ app.layout = layout
 register_callbacks(app)
 
 if __name__ == '__main__':
-    app.run_server(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
+    app.run_server(debug=True)
+
